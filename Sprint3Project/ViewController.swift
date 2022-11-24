0f085a5d19868157eaaf072f047cafe17c104a9e
KASAFF <91468100+KASAFF@users.noreply.github.com>
2022-11-24 17:52:42 +0300

    First version of counter app

--- Sprint3Project/ViewController.swift
+++ Sprint3Project/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var tapCounter: UILabel!
+    private var count: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor(red: 0.88, green: 0.82, blue: 0.74, alpha: 1.00)
+        tapCounter.text = "Значение счётчика: \(count)"
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        count += 1
+        tapCounter.text = "Значение счётчика: \(count)"
+    }
+    
 }
 
