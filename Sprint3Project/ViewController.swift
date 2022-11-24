232be8db4ad3031bc17edda7c280ba4402c8c3e5
KASAFF <91468100+KASAFF@users.noreply.github.com>
2022-11-24 17:04:17 +0300

    Initial Commit

---
+++ Sprint3Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sprint3Project
+//
+//  Created by Aleksey Kosov on 24.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
